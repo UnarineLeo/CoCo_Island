f09480d6ff90b8ba3ffe56b700f50bf4134211a8
UnarineLeo <unarineleo@gmail.com>
2024-08-12 16:03:07 +0200

    add xml writer code

--- Parser.java
+++ Parser.java
@@ -18,6 +18,55 @@ public class Parser {
         System.out.println("\u001B[32mSuccess\u001B[0m: Lexing Successful");
         System.out.println("Tokens" + tokens);
         System.out.println();
+        System.out.println("Open Tokens.xml file to view tokens");
+        writeXML();
+    }
+
+    private void writeXML() {
+        try {
+            FileWriter writer = new FileWriter("Tokens.xml");
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            printTree(tokens, writer, 0);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printTree(List<Token> tokens, FileWriter writer, int level) {
+
+        if (tokens.isEmpty()) {
+            try {
+                for (int i = 0; i < level; i++) {
+                    writer.write("\t");
+                }
+                writer.write("<Empty/>\n");
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            for (int i = 0; i < level; i++) {
+                writer.write("\t");
+            }
+
+            writer.write("<TokenStream>\n");
+            for(int i = 0; i < tokens.size(); i++)
+            {
+                writer.write("<Token>\n");
+                writer.write("<ID> " + tokens.get(i).getId() + " </ID>\n");
+                writer.write("<Type> " + tokens.get(i).getType() + " </Type>\n");
+                writer.write("<Content> " + tokens.get(i).getContent() + " </Content>\n");
+                writer.write("</Token>\n");
+            }
+            writer.write("</TokenStream>\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Node parse() {
